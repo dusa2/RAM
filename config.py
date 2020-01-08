32fccc5187583e185a8e923532ab0e228a2fe2b1
dusa2 <57438569+dusa2@users.noreply.github.com>
2020-01-08 11:46:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-win_size = 8                # The size of glimps
-batch_size = 32
+win_size = 16               # The size of glimps
+batch_size = 64
 loc_std = 0.22
 depth = 1
 hg_size = hl_size = 128     # The vector size for glimps network (before merge)
@@ -8,8 +8,8 @@ cell_output_size = 256
 loc_dim = 2                 # (batch_num, x_coor, y_coor)
 cell_size = 256
 cell_out_size = cell_size
-num_glimpses = 6            # The number of glimps that the paper use to get the lowest loss
-num_classes = 10            # MNIST has 10 classes
+num_glimpses = 8            # The number of glimps that the paper use to get the lowest loss
+num_classes = 3            # MNIST has 10 classes
 max_grad_norm = 5.
-step = 100000               # Epochs
+step = 200               # Epochs
 M = 10                      # Monte Carlo sampling
